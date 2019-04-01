c2ede1fa50d7f696fffa4fabb0b0a9a0da7f47a1
dbjerry <kimjitae91@gmail.com>
2019-04-01 23:28:21 +0900

    First React Study

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+  render(){
+    return(
+      <h1>hello this is a Movie app</h1>
+    )
+  }
+}
+
+export default Movie
