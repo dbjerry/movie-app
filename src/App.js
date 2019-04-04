f368e4558615d44b32857dfea12a346fe1c3fc5c
dbjerry <kimjitae91@gmail.com>
2019-04-05 01:08:33 +0900

    2019-04-05 01:08 ReactJS Study with nomad

--- src/App.js
+++ src/App.js
@@ -9,35 +9,52 @@ class App extends Component {
   state = {}
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then()
-    .catch(err => console.log(err))
+    this._getMoives()
   }
 
   // state를 변경하면 render는 다시 작동한다. 새로운 state와 함께
-  /*
-  componentDidMount() {
+  /* componentDidMount() {
     setTimeout(() => {
       this.setState({
         greeting: 'Hello again!'
       })
     }, 5000)
-  }
-  */
+  } */
 
   _renderMovies = () => {
-    const movies = [this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    const movies = [this.state.movies.map(movie => {
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.large_cover_image} 
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })]
     return movies
   }
 
+  _getMoives = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    // 성공적인 수행이 아니어도 작업이 완료되면 then을 부른다.
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
+      <div className={movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
-    );
+    )
   }
 }
 
