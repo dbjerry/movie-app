6f1b4192a2955d1f52fcf083461462780d4c3244
dbjerry <kimjitae91@gmail.com>
2019-04-05 01:06:47 +0900

    Updates

---
+++ precache-manifest.ab7027afdaa9aa0b8c601d2bd20dfadf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.927c110c.js"
+  },
+  {
+    "revision": "dec0b3ca535cd1332fd8",
+    "url": "/movie-app/static/js/main.a417d5a8.chunk.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/movie-app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "dec0b3ca535cd1332fd8",
+    "url": "/movie-app/static/css/main.54cd400d.chunk.css"
+  },
+  {
+    "revision": "5699f0dcb82974d77fac39dfbd3005a3",
+    "url": "/movie-app/index.html"
+  }
+];
