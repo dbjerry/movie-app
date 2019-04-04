b4e80bcbdc922f7d281734eb942b3f80adeb776d
dbjerry <kimjitae91@gmail.com>
2019-04-04 22:59:48 +0900

    2019-04-04 22:59 ReactJs Study with nomad

--- src/App.js
+++ src/App.js
@@ -2,34 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
-  }
-]
-
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
 
-  state = {
-    greeting: 'Hello!'
+  state = {}
+
+  componentDidMount() {
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then()
+    .catch(err => console.log(err))
   }
 
   // state를 변경하면 render는 다시 작동한다. 새로운 state와 함께
+  /*
   componentDidMount() {
     setTimeout(() => {
       this.setState({
@@ -37,15 +23,19 @@ class App extends Component {
       })
     }, 5000)
   }
+  */
+
+  _renderMovies = () => {
+    const movies = [this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })]
+    return movies
+  }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
-
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
+/*
 class Movie extends Component {
   
   static propTypes = {
@@ -30,6 +31,29 @@ class MoviePoster extends Component {
       <img src={this.props.poster}/>
     )
   }
+} */
+
+function Movie({title, poster}) {
+  return (
+    <div>
+      <MoviePoster poster={poster}/>
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
+function MoviePoster({poster}) {
+  return (
+    <img src={poster} alt="Movie Poster"/>
+  )
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
 }
 
 export default Movie
